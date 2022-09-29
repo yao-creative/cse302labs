78687a756f75788b607dd78734d3d4fdb08ead01
yiyaotan <yytan_work@gmail.com>
2022-09-29 14:23:17 +0200

    week 3 added

--- 3/starter/ast2tac.py
+++ 3/starter/ast2tac.py
@@ -1,106 +1,7 @@
 from re import search
 import sys
 import json
-
-#can assume that all of the variable names are distinct
-class Decl:
-    pass
-    # def __init__(self,position):
-    #     self.position = position 
-class DeclProc(Decl):
-    def __init__(self,name, arguments,returntype,body):
-        self.name = name
-        self.arguments = arguments
-        self.returntype = returntype
-        self.body = body
-    def __str__(self):
-        return "proc(%s,%s,%s,%s)" % (self.name,self.arguments,self.returntype,self.body)
-class Statement:
-    pass
-    # def __init__(self,position):
-    #     self.position = position
-class StatementVardecl(Statement):
-    def __init__(self,name, t, init):
-        self.name = name
-        self.type = t
-        self.init = init 
-    def __str__(self):
-        return "vardecl(%s,%s,%s)" % (self.name,self.type,self.init)
-class StatementPrint(Statement):
-    """Actually are are prints"""
-    def __init__(self,arguments):
-        self.arguments = arguments
-    def __str__(self):
-        return "print({})".format(self.arguments)
-class StatementAssign(Statement):
-    def __init__(self, lvalue,rvalue): 
-        self.lvalue = lvalue
-        self.rvalue = rvalue
-    def __str__(self):
-        return "StatementAssign(%s,%s)" % (self.lvalue,self.rvalue)
-class Expression:
-    pass
-    # def __init__(self, position):
-    #     self.position = position
-class ExpressionVar(Expression):
-    def __init__(self, name):
-        self.name = name
-    def __str__(self):
-        return "ExpressionVar({})".format(self.name)
-class ExpressionInt(Expression):
-    def __init__(self, value):
-        self.value = value
-    def __str__(self):
-        return "ExpressionInt({})".format(self.value)
-class ExpressionOp(Expression):
-    def __init__(self, operator, arguments):
-        """operator: string of the operator
-        arguments: list of expressions"""
-        self.operator = operator
-        self.arguments = arguments
-    def __str__(self):
-        return "ExpressionOp(%s,%s)" % (self.operator,self.arguments)
-
-
-def json_to_name(js_obj):
-    return js_obj[1]['value']
-def parse_type_from_str(string):
-    print(f"string: {string}")
-    return string[6:-1] # from after colon to before last >
-def json_to_expr(js_obj):
-    """Takes a json object and returns an expression"""
-    constructor = js_obj[0]
-    if constructor == "<decl:proc>":
-        name = json_to_name(js_obj[1]['name'])
-        arguments = js_obj[1]['arguments']
-        returntype = js_obj[1]['returntype']
-        body = [json_to_expr(obj) for obj in js_obj[1]['body']]
-        return DeclProc(name, arguments,returntype,body)
-    if constructor == "<statement:vardecl>":
-        name = json_to_name(js_obj[1]['name'])
-        t = parse_type_from_str(js_obj[1]['type'][0])
-        init = js_obj[1]['init'][1]["value"]
-        return StatementVardecl(name, t,init)
-    if constructor == "<statement:assign>":
-        lvalue_name =json_to_name(js_obj[1]["lvalue"][1]["name"])
-        lvalue  = ExpressionVar(lvalue_name)
-        rvalue = json_to_expr(js_obj[1]["rvalue"])
-        return StatementAssign(lvalue, rvalue)
-    if constructor == "<statement:eval>": #all are prints
-        return StatementPrint(json_to_expr(js_obj[1]["expression"][1]["arguments"][0]))
-    if constructor == "<expression:var>":
-        return ExpressionVar(json_to_name(js_obj[1]['name']))
-    if constructor == "<expression:int>":
-        return ExpressionInt(js_obj[1]['value'])
-    if constructor == "<expression:uniop>":
-        operator = json_to_name(js_obj[1]['operator'])
-        argument = json_to_expr(js_obj[1]['argument']) # recursive call
-        return ExpressionOp(operator, [argument]) # TODO: raise error if not unary
-    if constructor == "<expression:binop>":
-        operator = json_to_name(js_obj[1]['operator'])
-        left = json_to_expr(js_obj[1]['left']) # recursive call
-        right = json_to_expr(js_obj[1]['right'])
-        return ExpressionOp(operator,[left,right]) #TODO: raise error if not binary
+from bxast import *
 
 class TAC_proc:
     def __init__(self, tree, mm):
@@ -131,7 +32,7 @@ class TAC_proc:
     def new_temp(self, key=None):
         if key not in self.temps:
             self.temps[key] = f"%{len(self.temps)}"
-            print(F"created new temp: {self.temps[key]}")
+            # print(F"created new temp: {self.temps[key]}")
         else:
             print(F"temp already exists: {self.temps[key]}")
         return self.temps[key]
@@ -144,16 +45,15 @@ class TAC_proc:
     def save(self,outfile):
         with open(outfile, 'w') as fp:
             json.dump([{"proc": self.proc, "body": self.body}], fp)
+
     #___________________________________________________________
     #TMM
     #___________________________________________________________
     def tmm_stmt(self,statement):
         """Takes a statement and returns a list of TAC statements"""
         if isinstance(statement, StatementVardecl):
-            opcode = "const"
-            args = [statement.init] #initialization value of declaration
-            result = self.new_temp(statement.name) #use the name of the variable as the key
-            return [TAC_line(opcode, args, result).format()]
+            result = self.new_temp(statement.name)
+            return self.tmm_expr(statement.init,result) 
         elif isinstance(statement, StatementPrint):
             temp_to_print = self.new_temp(f"{len(self.temps)}")
             #this is guaranteed unique since it is an integer and length keeps growing.
@@ -162,13 +62,9 @@ class TAC_proc:
             result = None
             return prev_lines + [TAC_line(opcode, [temp_to_print], result).format()]
         elif isinstance(statement, StatementAssign):
-            if self.search(statement.lvalue.name):
-                result = self.temps[statement.lvalue.name]
-            else:
-                result = self.new_temp(statement.lvalue.name)
+            result = self.new_temp(statement.lvalue)
             return self.tmm_expr(statement.rvalue,result)
             
-
             
     def tmm_expr(self,expression,result):
         """Takes an expression and returns a list of TAC statements"""
@@ -200,8 +96,9 @@ class TAC_proc:
     def tmm(self):
         print(f"tmm")
         for item in self.tree.body:
+            print(f"item: {item}")
             self.body += self.tmm_stmt(item)
-        print(f"body: {self.body}")
+        # print(f"body: {self.body}")
         
     #___________________________________________________________
     #BMM
@@ -209,10 +106,9 @@ class TAC_proc:
     def bmm_stmt(self,statement):
         """Takes a statement and returns a list of TAC statements"""
         if isinstance(statement, StatementVardecl):
-            opcode = "const"
-            args = [statement.init] #initialization value of declaration
-            result = self.new_temp(statement.name) #use the name of the variable as the key
-            return [TAC_line(opcode, args, result).format()]
+            prev_lines, expr_result = self.bmm_expr(statement.init)
+            result = self.new_temp(statement.name)
+            return prev_lines + [TAC_line("copy", [expr_result], result).format()]
         elif isinstance(statement, StatementPrint):
             #this is guaranteed unique since it is an integer and length keeps growing.
             prev_lines,temp_to_print = self.bmm_expr(statement.arguments)
@@ -220,12 +116,8 @@ class TAC_proc:
             result = None
             return prev_lines + [TAC_line(opcode, [temp_to_print], result).format()]
         elif isinstance(statement, StatementAssign):
-            print(f"expression assign")
             l, subexpr_result = self.bmm_expr(statement.rvalue)
-            if self.search(statement.lvalue.name):
-                result = self.temps[statement.lvalue.name]
-            else:
-                result = self.new_temp(statement.lvalue.name)
+            result = self.new_temp(statement.lvalue)
             #copy on assign
             opcode = "copy"
             args = [subexpr_result]
@@ -241,7 +133,7 @@ class TAC_proc:
             result = self.new_temp(f"{len(self.temps)}")
             return [TAC_line(opcode, args,result).format()], result
         elif isinstance(expression, ExpressionVar):
-            print(f"expression var: {expression.name}")
+            # print(f"expression var: {expression.name}")
             return [], self.temps[expression.name]
         elif isinstance(expression, ExpressionOp) and len(expression.arguments) == 1:
             #unary operator
@@ -263,9 +155,9 @@ class TAC_proc:
     def bmm(self):
         print(f"bmm")
         for item in self.tree.body:
-            print(f"line: {item}")
+            # print(f"line: {item}")
             self.body += self.bmm_stmt(item)
-        print(f"body: {self.body}")
+        # print(f"body: {self.body}")
 
 class TAC_line:
     def __init__(self, opcode, args, result):
@@ -274,19 +166,3 @@ class TAC_line:
         self.result = result
     def format(self):
         return {"opcode": self.opcode, "args": self.args, "result": self.result}
-
-
-
-if __name__ == "__main__":
-    infile = sys.argv[1]
-    with open(infile, 'r') as fp:
-        js_obj = json.load(fp)
-        js_obj = js_obj["ast"][0]
-
-    tree =json_to_expr(js_obj)
-    print(f"tree: {tree}")
-    mm = sys.argv[2]
-    TAC_proc(tree, mm).save(sys.argv[3])
-    
-#TMM
-#---------------------------------------------------
---
+++ 3/starter/scanner.py
@@ -0,0 +1,79 @@
+import ply.lex as lex
+import argparse
+import sys
+#words 
+reserved = {
+    "main" : "MAIN",
+    "while": "WHILE",
+    "print": "PRINT",
+    "def" : "DEF",
+    "int" : "INT",
+    "var" : "VAR",
+    "if" : "IF",
+    "else": "ELSE",
+    "while": "WHILE"
+}
+
+#Symbols
+#check to use NEGATIVE == UNARY MINUS
+tokens = ("PLUS", "MINUS", "SEMICOLON", "COLON", "LPAREN", "RPAREN", "IDENT", "NUMBER", 
+        "LBRACE", "RBRACE", "EQUALS", "MULTIPLY", "DIVIDE",
+         "PERCENT", "BITWISE_AND", "BITWISE_OR", "BITWISE_XOR",
+         "BITWISE_NEGATION", "LOGICAL_SHIFT_LEFT", "LOGICAL_SHIFT_RIGHT") + tuple(reserved.values())
+
+#Non identifiers and numbers:
+
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_SEMICOLON = r'\;'
+t_COLON = r'\:'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
+t_EQUALS = r'\='
+t_MULTIPLY = r'\*'
+t_DIVIDE = r'/'
+t_PERCENT = r'\%'
+t_BITWISE_AND = r'\&'
+t_BITWISE_OR = r'\|'
+t_BITWISE_XOR = r'\^'
+t_BITWISE_NEGATION = r'\~'
+t_LOGICAL_SHIFT_LEFT = r'\<\<'
+t_LOGICAL_SHIFT_RIGHT = r'\>\>'
+
+
+def t_IDENT(t):
+    r'[A-Za-z][A-Za-z0-9_]*'
+    t.type = reserved.get(t.value, 'IDENT')
+    return t
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+t_ignore_COMMENT = r'//.*\n?'
+t_ignore = ' \t'
+
+def t_error(t):
+    print(f"Lexing error: Illegal Character '{t.value[0]}' on line {t.lexer.lineno} ")
+    t.lexer.skip(1) 
+
+def t_newline(t):
+    r'\n'
+    t.lexer.lineno +=1
+
+
+    
+if __name__ == "__main__":
+    
+    # parser = argparse.ArgumentParser(description= "Lex files")
+    # parser.add_argument("--")
+    lexer = lex.lex()
+    file_to_lex = sys.argv[1]
+    with open(file_to_lex, "r") as infile:
+        for line in infile:
+            lexer.input(line)
+            for token in lexer:
+                print(token)
