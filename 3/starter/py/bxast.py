969322a6ad9ef102e950e96c777575e2cf0b39ef
vrushank-agrawal <vrushank2001@gmail.com>
2022-10-06 17:07:11 +0200

    add types and update ExprOp class

--- 3/starter/py/bxast.py
+++ 3/starter/py/bxast.py
@@ -1,11 +1,40 @@
-#can assume that all of the variable names are distinct
-binops = ["addition", "substraction", "multiplication",
-          "division", "modulus", "bitwise-and", "bitwise-or", 
-          "bitwise-xor", "logical-shift-left", "logical-shift-right",
-          "cmpe", "cmpne", "cmpl", "cmple", "cmpge",
-          "cmpg", "logical-and", "logical-or"]
-          
-unops = [ "bitwise-negation", "opposite", "not"]
+from typing import List, Tuple, Dict
+
+_binops_int: tuple = ("addition", "substraction", "multiplication",
+                    "division", "modulus", "bitwise-and", "bitwise-or", 
+                    "bitwise-xor", "logical-shift-left", "logical-shift-right")
+_binops_cmp_val: tuple = ("cmpl", "cmple", "cmpge", "cmpg")
+_binops_cmp: tuple = ("cmpe", "cmpne")
+_binops_bool: tuple = ("logical-and", "logical-or")
+_binops: tuple = _binops_bool + _binops_cmp_val + _binops_cmp + _binops_int
+
+_unops_int: tuple = ("bitwise-negation", "opposite")
+_unops_bool: tuple = ("not")
+_unops: tuple = _unops_bool + _unops_int
+
+# ------------------------------------------------------------------------------#
+# Macro Classes
+# ------------------------------------------------------------------------------#
+
+class BX_TYPE:
+    """ Class of all the data types in BX """
+    class INT: 
+        """ Class of INT type """
+        def __str__(self) -> str:
+            return "int"
+
+    class BOOL: 
+        """ Class of BOOL type """
+        def __str__(self) -> str:
+            return "bool"
+
+class Scopes:
+    pass
+
+
+# ------------------------------------------------------------------------------#
+# Top Classes
+# ------------------------------------------------------------------------------#
 
 import copy
 
@@ -18,7 +47,6 @@ class Node:
         msg = f"\033[0;37m in line {self.location[0]}\n {error}"
         raise SyntaxError(msg)
 
-        
 class DeclProc(Node):
     def __init__(self,location, name, arguments,returntype,body, previous_functions = []):
         super().__init__(location)
@@ -26,103 +54,24 @@ class DeclProc(Node):
         self.arguments = arguments
         self.returntype = returntype
         self.body = body
-    def syntax_check(self):
+    
+    def type_check(self) -> None:
         if self.name != "main":
             self.syntax_error("non-main function found")
         if self.arguments != []:
-            self.syntax_error("Error: main function cannot have arguments")
+            self.syntax_error(" main function cannot have arguments")
         if self.returntype != None:
-            self.syntax_error("Error: main function cannot have a return type")
+            self.syntax_error(" main function cannot have a return type")
         for statement in self.body:
-            statement.syntax_check()
+            statement.type_check()
+    
     def __str__(self):
         return "proc(%s,%s,%s,%s)" % (self.name,self.arguments,self.returntype,self.body)
 
-class Statement(Node):
-    def __init__(self,location):
-        super().__init__(location)
 
-class StatementBlock(Statement):
-    def __init__(self,location, statements):
-        super().__init__(location)
-        self.statements = statements
-    def syntax_check(self):
-        for statement in self.statements:
-            statement.syntax_check()
-    def __str__(self):
-        return "block(%s)" % (self.statements)
-
-class StatementVardecl(Statement):
-    def __init__(self,location, name, ty, init, declared_ids = []):
-        super().__init__(location)
-        self.name = name
-        self.ty = ty
-        self.init = init 
-        self.declared_ids = copy.deepcopy(declared_ids)
-    def __str__(self):
-        return "vardecl(%s,%s,%s)" % (self.name,self.ty,self.init)
-
-    def syntax_check(self):
-        if self.name in self.declared_ids:
-            self.syntax_error("Error: variable already declared")
-        self.init.syntax_check()
-        
-class StatementPrint(Statement):
-    """Actually are are prints"""
-    def __init__(self, location, arguments):
-        super().__init__(location)
-        self.arguments = arguments
-    def __str__(self):
-        return "print({})".format(self.arguments)
-    def syntax_check(self):
-        self.arguments.syntax_check()
-
-class StatementAssign(Statement):
-    def __init__(self, location, lvalue,rvalue, declared_ids = []): 
-        super().__init__(location)
-        self.lvalue = lvalue
-        self.rvalue = rvalue
-        self.declared_ids = copy.deepcopy(declared_ids)
-    def __str__(self):
-        return "StatementAssign(%s,%s)" % (self.lvalue,self.rvalue)
-    def syntax_check(self):
-        if self.lvalue not in self.declared_ids:
-            self.syntax_error("Error: variable yet not declared")
-        self.rvalue.syntax_check()
-
-class StatementIfElse(Statement):
-    def __init__(self, location, condition, block, ifrest):
-        super().__init__(location)
-        """if_body is a block, condition is an expression"""
-        self.condition = condition
-        self.block = block 
-        self.if_rest = ifrest
-    def __str__(self):
-        return "ifelse(%s,%s,%s)" % (self.condition,self.block,self.if_rest)
-    def syntax_check(self):
-        self.condition.syntax_check()
-        self.block.syntax_check()
-        self.if_rest.syntax_check()
-
-class StatementWhile(Statement):
-    def __init__(self, location, condition, block):
-        super().__init__(location)
-        self.condition = condition
-        self.block = block
-    def __str__(self):
-        return "while(%s,%s)" % (self.condition,self.block)
-    def syntax_check(self):
-        self.condition.syntax_check()
-        self.block.syntax_check()
-
-class StatementJump(Statement):
-    def __init__(self, location, keyword):
-        super().__init__(location)
-        self.keyword = keyword
-    def __str__(self):
-        return "Jump(%s)" % (self.keyword)
-    def syntax_check(self):
-        pass
+# ------------------------------------------------------------------------------#
+# Expression Classes
+# ------------------------------------------------------------------------------#
 
 class Expression(Node):
     def __init__(self,location):
@@ -132,11 +81,13 @@ class ExpressionBool(Expression):
     def __init__(self,location, value):
         super().__init__(location)
         self.value = value
+    
     def __str__(self):
         return "bool(%s)" % (self.value)
-    def syntax_check(self):
+    
+    def type_check(self) -> None:
         if self.value not in [True, False]:
-            self.syntax_error("Error: value must be 'true' or 'false'.")
+            self.syntax_error(" value must be 'true' or 'false'.")
 
 class ExpressionVar(Expression):
     def __init__(self, location, name, declared_ids = []):
@@ -148,43 +99,180 @@ class ExpressionVar(Expression):
     def __str__(self):
         return "ExpressionVar({})".format(self.name)
 
-    def syntax_check(self):
+    def type_check(self) -> None:
         if self.name not in self.declared_ids:
-            self.syntax_error("Error: variable yet not declared")
+            self.syntax_error(" variable yet not declared")
 
 class ExpressionInt(Expression):
     def __init__(self, location, value):
         super().__init__(location)
         self.value = value
+        self._max = 1<<63
+    
     def __str__(self):
         return "ExpressionInt({})".format(self.value)
-    def syntax_check(self):
+    
+    def type_check(self) -> None:
         if self.value < 0:
-            self.syntax_error("Error: negative number")
-        if self.value >= 2**63:
-            self.syntax_error("Error: number too large")
+            self.syntax_error(" negative number")
+        if self.value >= self._max:
+            self.syntax_error(" number too large")
 
 class ExpressionOp(Expression):
     def __init__(self, location, operator, arguments):
         """operator: string of the operator
         arguments: list of expressions"""
         super().__init__(location)
-        self.operator = operator
+        self.operator: str = operator
         self.arguments = arguments
+        self.result_type: BX_TYPE = None
+        self.argument_type: Tuple[BX_TYPE] = None
+        self._type_init()
+    
     def __str__(self):
         return "ExpressionOp(%s,%s)" % (self.operator,self.arguments)
 
-    def syntax_check(self):
-        if self.operator in binops:
-            if len(self.arguments) != 2:
-                self.syntax_error("Error: arithmetic operator takes two arguments")
-            self.arguments[0].syntax_check()
-            self.arguments[1].syntax_check()
-        elif self.operator in unops:
-            if len(self.arguments) != 1:
-                self.syntax_error("Error: negation operator takes one argument")
-            self.arguments[0].syntax_check()
-        else:
-            self.syntax_error("Error: invalid operator")
-        for arg in self.arguments:
-            arg.syntax_check()
+    def _type_init(self) -> None:
+        """ Initializes the result and argument type based on argument input """
+        if self.operator in _binops_int:
+            self.argument_type = (BX_TYPE.INT, BX_TYPE.INT)
+            self.result_type = BX_TYPE.INT
+        
+        elif self.operator in _binops_cmp_val:
+            self.argument_type = (BX_TYPE.INT, BX_TYPE.INT)
+            self.result_type = BX_TYPE.BOOL
+        
+        elif self.operator in _binops_cmp:
+            self.argument_type = (None, None)
+            self.result_type = BX_TYPE.BOOL
+        
+        elif self.operator in _binops_bool:
+            self.argument_type = (BX_TYPE.BOOL, BX_TYPE.BOOL)
+            self.result_type = BX_TYPE.BOOL
+        
+        elif self.operator in _unops_int:
+            self.argument_type = (BX_TYPE.INT)
+            self.result_type = BX_TYPE.INT
+
+        elif self.operator in _unops_bool:
+            self.argument_type = (BX_TYPE.BOOL)
+            self.result_type = BX_TYPE.BOOL
+
+        else:       # this should not happen
+            self.syntax_error(f"Unkown operator {self.operator}")
+
+
+    def type_check(self) -> None:
+        if len(self.arguments) != len(self.argument_type):
+            self.syntax_error(f"{self.operator} takes {len(self.argument_type)} arguments got {len(self.arguments)}")
+
+        for index, arg in enumerate(self.arguments):
+            arg.type_check()
+        
+# ------------------------------------------------------------------------------#
+# Statement Classes
+# ------------------------------------------------------------------------------#
+
+class Statement(Node):
+    def __init__(self,location):
+        super().__init__(location)
+
+class StatementBlock(Statement):
+    def __init__(self,location, statements):
+        super().__init__(location)
+        self.statements = statements
+    
+    def type_check(self) -> None:
+        for statement in self.statements:
+            statement.type_check()
+    
+    def __str__(self):
+        return "block(%s)" % (self.statements)
+
+class StatementVardecl(Statement):
+    def __init__(self,location, name, ty, init, declared_ids = []):
+        super().__init__(location)
+        self.name = name
+        self.ty = ty
+        self.init = init 
+        self.declared_ids = copy.deepcopy(declared_ids)
+    
+    def __str__(self):
+        return "vardecl(%s,%s,%s)" % (self.name,self.ty,self.init)
+
+    def type_check(self) -> None:
+        if self.name in self.declared_ids:
+            self.syntax_error(" variable already declared")
+        self.init.type_check()
+        
+class StatementPrint(Statement):
+    """Actually are are prints"""
+    def __init__(self, location, arguments):
+        super().__init__(location)
+        self.arguments = arguments
+    
+    def __str__(self):
+        return "print({})".format(self.arguments)
+    
+    def type_check(self) -> None:
+        self.arguments.type_check()
+
+class StatementAssign(Statement):
+    def __init__(self, location, lvalue,rvalue, declared_ids = []): 
+        super().__init__(location)
+        self.lvalue = lvalue
+        self.rvalue = rvalue
+        self.declared_ids = copy.deepcopy(declared_ids)
+    
+    def __str__(self):
+        return "StatementAssign(%s,%s)" % (self.lvalue,self.rvalue)
+    
+    def type_check(self) -> None:
+        if self.lvalue not in self.declared_ids:
+            self.syntax_error(" variable yet not declared")
+        self.rvalue.type_check()
+
+# ------------------------------------------------------------------------------#
+# Conditional Statement Classes
+# ------------------------------------------------------------------------------#
+
+class StatementIfElse(Statement):
+    def __init__(self, location, condition, block, ifrest):
+        super().__init__(location)
+        """if_body is a block, condition is an expression"""
+        self.condition = condition
+        self.block = block 
+        self.if_rest = ifrest
+    
+    def __str__(self):
+        return "ifelse(%s,%s,%s)" % (self.condition,self.block,self.if_rest)
+    
+    def type_check(self) -> None:
+        self.condition.type_check()
+        self.block.type_check()
+        self.if_rest.type_check()
+
+class StatementWhile(Statement):
+    def __init__(self, location, condition, block):
+        super().__init__(location)
+        self.condition = condition
+        self.block = block
+    
+    def __str__(self):
+        return "while(%s,%s)" % (self.condition,self.block)
+    
+    def type_check(self) -> None:
+        self.condition.type_check()
+        self.block.type_check()
+
+class StatementJump(Statement):
+    def __init__(self, location, keyword):
+        super().__init__(location)
+        self.keyword = keyword
+    
+    def __str__(self):
+        return "Jump(%s)" % (self.keyword)
+    
+    def type_check(self) -> None:
+        pass
+
