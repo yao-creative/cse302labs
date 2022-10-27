06f33ef0ee8f29aeae64dc9310052cc6b0e984e1
vrushank-agrawal <vrushank2001@gmail.com>
2022-10-27 17:38:39 +0200

    finish uce and update coalesce

--- 4/starter/py/cfg.py
+++ 4/starter/py/cfg.py
@@ -1,4 +1,5 @@
-from typing import List, Set, Tuple, Union
+from operator import imod
+from typing import List, Set, Dict, Union
 
 # ------------------------------------------------------------------------------#
 # Block Class
@@ -14,13 +15,9 @@ class Block:
 
     def __init__(self, instr: List[dict]) -> None:
         self.__instrs: List[dict] = instr
-        self.__label_dest: Set[str] = set()
-        self.__succ: Union[str, False] = list(self.__label_dest[0])[0] if len(self.__label_dest) == 1 else False
-        self.__successor_labels()
-
-    def __hash__(self) -> int:
-        """ Used for dict storing of pred values in CFG class """
-        return hash(self.__label_dest)
+        self.__successors: list = []
+        self.__update_successors()
+        self.__pred: list = []
 
     # ---------------------------------------------------------------------------#
     # Instr helpers
@@ -51,23 +48,39 @@ class Block:
     # ---------------------------------------------------------------------------#
     # Label helpers
 
-    def __successor_labels(self) -> None:
-        """ Returns all label destinations for jcc instr in the block """
-        dest = set()
-        for instr in self.__instrs:
-            if instr["opcode"] in self.jccs:
-                dest.append(instr["args"][-1])
-        self.__label_dest = dest
-
-    def successors(self) -> Set[str]:
-        """ Returns all jcc label dests for current block """
-        self.__successor_labels()
-        return self.__label_dest
-
     def block_label(self) -> str:
         """ Returns the name of the block's label """
         return self.__instrs[0]["args"]
 
+    def __update_successors(self) -> None:
+        """ updates all successors for the block """
+        dest = []
+        for instr in self.__instrs:
+            if instr["opcode"] in self.jccs:
+                if instr["args"][-1] not in dest:
+                    dest.append(instr["args"][-1])
+        self.__successors = dest
+
+    def set_pred(self, preds: list) -> str:
+        """ Sets the predecessor for the curr block """
+        self.__pred = preds
+
+    def ret_successors(self) -> list:
+        """ Returns all successors for current block """
+        self.__update_successors()
+        return self.__successors
+
+    def ret_predecessors(self) -> list:
+        """ Returns all predecessors for current block """
+        return self.__pred
+
+    def has_one_succ(self) -> bool:
+        """ If block has one succ return True """
+        return len(self.__successors) == 1
+
+    def has_one_pred(self) -> bool:
+        """ If blokc has one pred return True """
+        return len(self.__pred) == 1
 
 # ------------------------------------------------------------------------------#
 # CFG Class
@@ -78,11 +91,12 @@ class CFG:
 
     def __init__(self, blocks: List[Block]) -> None:
         self.__blocks: List[Block] = blocks
+        self.__num_blocks: int = len(self.__blocks)
         self.__entry_block: Block = self.__blocks[0]
-        self.__edges: dict = dict()
+        self.__successors: Dict[str, set] = dict()
         self.__update_next()
-        # blocks needed to be called by labels for serialization
-        self.__blocks_by_labels: dict = {block.block_label(): block for block in self.__blocks}
+        self.__update_prev()
+        self.__labels_to_blocks: Dict[str, Block] = {block.block_label(): block for block in self.__blocks}
         self.__predecessors: dict = {block.block_label(): [] for block in self.__blocks}
 
     # ---------------------------------------------------------------------------#
@@ -93,24 +107,27 @@ class CFG:
         edges = {}
         for block in self.__blocks:
             edges[block.block_label()] = block.successors()
-        self.__edges = edges
-
-    def __next(self, block: Block) -> List[Block]:
-        """ Returns the successor blocks of the current block """
-        succ_labels = self.__edges[block.block_label()]
-        blocks = [self.__blocks_by_labels[lab] for lab in succ_labels]
-        return blocks
+        self.__successors = edges
 
     def __update_prev(self) -> None:
-        """ Updates the predecessor graph """
+        """ Updates the predecessor graph {label: [label]}"""
+        pred_graph = dict()
         for block in self.__blocks:
             for label in block.successors():
-                self.__predecessors[label] += block.block_label()
+                pred_graph[label] += block.block_label()
+        self.__predecessors = pred_graph
+        for lab, preds in pred_graph.items():
+            self.__labels_to_blocks[lab].set_pred(preds)
+
+    def __next(self, label: str) -> List[str]:
+        """ Returns the successor labels of the current label """
+        self.__update_next()
+        return self.__successors[label]
 
     def __prev(self, block: Block) -> List[Block]:
         """ Returns the predecessor blocks for the current block """
         pred_labels = self.__predecessors[block.block_label()]
-        blocks = [self.__blocks_by_labels[lab] for lab in pred_labels]
+        blocks = [self.__labels_to_blocks[lab] for lab in pred_labels]
         return blocks
 
     def __coalesce_blocks(self, block1: Block, block2: Block) -> Block:
@@ -119,15 +136,49 @@ class CFG:
         block1.add_instrs(block2.instructions())
         return block1
 
+    def __coalescable(self, block1: Block, block2: Block) -> bool:
+        """ Checks if given blocks can be coalesced """
+        if block1.has_one_succ() and block2.has_one_pred():
+            if block1.ret_successors()[0] == block2.ret_predecessors()[0]:
+                return True
+        return False
+
+    def __del_block(self, block: Block) -> None:
+        """ Delete the given block """
+        self.__blocks.remove(block)
+        del self.__successors[block.block_label()]
+        self.__update_next()
+        self.__entry_block = self.__blocks[0]
+
     # ---------------------------------------------------------------------------#
     # CFG Operations
 
     def __uce(self) -> None:
         """ Unreachable Code Elimination """
+        visited_blocks = {self.__entry_block.block_label()}
+        to_visit = self.__entry_block.ret_successors()
+        while len(to_visit) > 0:
+            label = to_visit.pop()
+            if label not in visited_blocks:
+                visited_blocks.add(label)
+                to_visit.extend(self.__next(label))
 
+        to_delete = []
+        for block in self.__blocks:
+            if block.block_label() not in visited_blocks:
+                to_delete.append(block)
+        for block in to_delete:
+            self.__del_block(block)
 
     def __coalescing(self) -> None:
         """ Colaesce two blokcs if one succ and one pred """
+        self.__update_prev()
+        index = 0
+        while index < self.__num_blocks:
+            block = self.__blocks[index]
+            next_block = self.__blocks[index+1]
+            if self.__coalescable(block, next_block):
+                new_block = self.__coalesce_blocks(block, next_block)
 
 
     # ---------------------------------------------------------------------------#
@@ -143,7 +194,7 @@ class CFG:
         while curr_block is not None:
             scheduled.append(curr_block)
             for label in next_labels:
-                successors.add(self.__blocks_by_labels(label))
+                successors.add(self.__labels_to_blocks(label))
             if successors:
                 curr_block = successors.pop()
                 next_labels = curr_block.successors()
