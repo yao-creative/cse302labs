d27baae663ac81a64b9c0b36a1a64c4a8260d479
vrushank-agrawal <vrushank2001@gmail.com>
2022-11-10 22:41:14 +0100

    add examples from lec6

--- 4/starter/py/bx_runtime.c
+++ 4/starter/py/bx_runtime.c
@@ -1,8 +1,17 @@
-
 #include <stdio.h>
 #include <stdint.h>
 
-/* Note: TAC int == C int64_t
-This is because C int is usually only 32 bits. */
-void __bx_print_int(int64_t x) { printf("%ld\n", x); }
-void __bx_print_bool(int64_t b) { printf(b == 0 ? "false\n" : "true\n"); }
+void __bx_print_int(int64_t x)
+{
+    printf(”%ld\n”, x); /* same as lab 3 (except for name) */
+}
+
+/* note: booleans are converted into int64_ts when
+being passed by value to function calls: more on that
+next week */
+
+void __bx_print_bool(int64_t x)
+{
+    /* boolean encoding: 0 is false, everything else is true */
+    printf(”%s\n”, x == 0 ? ”false” : ”true”);
+}
